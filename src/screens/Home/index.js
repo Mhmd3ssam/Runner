7f7a921ad42ec1096298ead10b4521398995428e
MohamedEssam1695 <toma126542@gmail.com>
2022-01-12 22:10:33 +0200

    add toggle- from distance to time -  button from  to the mock run/home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -12,7 +12,7 @@ const Home = () => {
                 marginTop: 50,
                 marginBottom: 50
             }}
-                onPress={() => { console.log('Hi') }}
+                onPress={() => { console.log('My distance') }}
             >
                 <Text style={{
                     fontSize: 42,
@@ -41,24 +41,46 @@ const Home = () => {
                 }}
             >
             </MapView>
-            <Avatar
-                size={115}
-                rounded
-                title="START"
-                titleStyle={{
-                    fontSize:22,
-                    color:"#fff",
-                    fontWeight:"bold"
+            {/*Start Button  */}
+            <View style={{
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginBottom: 10,
+                marginTop: 10,
+            }}>
+                <Avatar
+                    size={115}
+                    rounded
+                    title="START"
+                    titleStyle={{
+                        fontSize: 22,
+                        color: "#fff",
+                        fontWeight: "bold"
 
-                }}
-                containerStyle={{
-                    backgroundColor: '#fe9836',
-                    marginBottom: 50,
-                    marginTop: 50,
-                    fontSize:1
-                }}
-                onPress={()=>{console.log('clicked')}}
-            />
+                    }}
+                    containerStyle={{
+                        backgroundColor: '#fe9836',
+                        fontSize: 1
+                    }}
+                    onPress={() => { console.log('start') }}
+                />
+                {/* Toggle butoon to conter the trip by distance or time  */}
+                <Pressable
+                    onPress={() => { console.log('change to time') }}
+                    style={{
+                        marginBottom: 10,
+                        marginTop: 10,
+                    }}
+                >
+                    <Text style={{
+                        alignSelf: "center",
+                        fontSize: 15,
+                    }}>
+                        {'Distance'}
+                    </Text>
+                </Pressable>
+            </View>
+
         </View>
     )
 };
