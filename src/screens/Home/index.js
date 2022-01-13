dd7ee34c459274ae3233ad9aa4d4c5fbc95076ac
MohamedEssam1695 <toma126542@gmail.com>
2022-01-13 04:38:16 +0200

    finsh valadation for user inputs in run/home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,29 +1,52 @@
-import React,{useState} from 'react';
-import { View, Text, Pressable, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Pressable, TextInput } from 'react-native';
 import MapView from 'react-native-maps';
 import { Styles } from './styles';
 
 import { Avatar } from 'react-native-elements';
 
 const Home = () => {
-    const[distance,setDistance] = useState("0.1");
-    const[toggle,setToggle] = useState(true);
-    const [error,setError] = useState(null);
+    const [distance, setDistance] = useState('0.0');
+    const [time, setTime] = useState({
+        hours:'00',
+        minutes:'00'
 
-    const inputValdation = (input)=>{
-        let rgx = /^[0-9]*\.?[0-9]?$/;
-        return input.match(rgx);
+    })
+    const [toggle, setToggle] = useState(true);
+    const [error, setError] = useState(null);
+
+    const inputValdation = (input, typeOFMeasure) => {
+        let rgx ;
+        if (typeOFMeasure) {
+            rgx = /^[0-9]*\.?[0-9]?$/;
+            return [input.match(rgx),typeOFMeasure];
+        }else{
+            rgx = /^[0-9]{0,2}:{1}[0-9]{0,2}?$/;
+            return [input.match(rgx),typeOFMeasure];
+        }
+
     }
 
-    const inputHandelChange = (value)=>{
-        if(!inputValdation(value)){
-            setDistance(distance);
-            setError('UnVaild Number');
-        }else{
-            setError("");
+    const inputHandelChange = (value, toggle) => {
+
+        if (!inputValdation(value, toggle)[0] && toggle) {
+            setDistance('');
+            setError('Be ease 😎');
+        } else if (!inputValdation(value, toggle)[0] || toggle){
             setDistance(value);
+            setError('')
+        }else if(!inputValdation(value, toggle)[0] && !toggle){
+            setTime('');
+            setError('Be ease 😎');
+        }else {
+            let h = value.split(':')[0];
+            let m = value.split(':')[1];
+            setTime({
+                hours:h,
+                minutes:m
+            });
         }
-        
+
     }
 
     return (
@@ -32,14 +55,14 @@ const Home = () => {
             <Pressable style={Styles.distanceContainer}
                 onPress={() => { console.log('My distance') }}
             >
-                <TextInput style={{...Styles.distanceContent, color: error ?"red":"black"}} 
-                value={distance} 
-                keyboardType='decimal-pad'
-                onChangeText={(value)=>{inputHandelChange(value)}}
+                <TextInput style={Styles.distanceContent}
+                    value={toggle ? distance : `${time.hours}:${time.minutes}`}
+                    keyboardType='decimal-pad'
+                    onChangeText={(value) => { inputHandelChange(value, toggle) }}
                 />
                 <View style={Styles.underLineDistanceContent}></View>
-                <Text style={{...Styles.measurementUnit ,color: error ?"red":"black" }}>
-                    {error ?error:toggle?"Kilometers":"Minutes"}
+                <Text style={{ ...Styles.measurementUnit, color: error ? "red" : "black" }}>
+                    {error ? error : toggle ? "Kilometers" : "Hours : Minutes"}
                 </Text>
             </Pressable>
             {/* Map section */}
@@ -65,11 +88,14 @@ const Home = () => {
                 />
                 {/* Toggle butoon to conter the trip by distance or time  */}
                 <Pressable
-                    onPress={() => {setToggle(!toggle) }}
-                    style={Styles.toggleContainer}   
+                    onPress={() => { 
+                        setToggle(!toggle);
+                        setError('')
+                     }}
+                    style={Styles.toggleContainer}
                 >
                     <Text style={Styles.toggleContent}>
-                        {toggle?'Distance':'Time'}
+                        {toggle ? 'Distance' : 'Time'}
                     </Text>
                 </Pressable>
             </View>
